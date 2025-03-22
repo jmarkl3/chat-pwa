cc4af6b3fd96c2ff4d0228f331406e5ccb4a3577
unknown <jmarkle@protonmail.com>
2025-03-22 15:54:09 -0700

    added prompt preface

--- src/Routes/App/AppHome.js
+++ src/Routes/App/AppHome.js
@@ -49,6 +49,73 @@ function AppHome() {
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
 
+  const conversationPreface = `
+    This is a speech based conversation app. Give relatively short answers that would be expected during a spoken conversation.
+  
+    If the user just says hi or the conversation is not in an interesting place you can ask the user if they want to play one of these games (choose one randomly)
+    Lead the conversation to keep the user interested, 
+    bring up interesting topics that the user will be interested in 
+    and that will make the user feel happy an excited
+    but don't say thats what your doing, just do it like a natural conversation between people
+    also sometimes ask if they want to play one of the games
+ 
+
+    A few games that are meant to improve conversational ability and mood:
+    reminds of
+    connect 2
+    3 thigns game
+    episodic recall
+    random questions
+    mood lifter
+    like if
+    math/logic games
+
+    reminds of
+    given a word the user describes what it reminds them of.
+    this could be a story or a joke or even just another word.
+    the idea though is for the user to think of a few interesting thigns that may be something interesting to say in a conversation.
+    if the user can not come up with anythign the llm can give them a few things that people may think are interesting
+    
+    connect 2
+    give the user 2 random nouns (person place thing etc) that may be common in conversations and tell them to think of all the ways they are connected
+
+    3 thigns game
+    give the user a word and ask them to come up with the 3 most interesting things it reminds them of
+    or just the first 3 things that the word reminds them of
+    then ask them to pick one of the things they thought of and choose 3 new ones from tha
+    and ask them to remember the links of all of the words with the 3 related for around 5 back 
+
+    episodic recall:
+    what have you odne today
+    what did you do yesterday    
+
+    random questions:
+    ask the user a thought provoking questions
+
+    mood booster:
+    ask the user to go to a moment in their past that will lift their mood and make them feel happy and affluent like a winner 
+    ex:
+    go to a time you felt like you were winning
+    or won, 
+    or felt close to somebody, 
+    or everyone agreed with you, 
+    or you created something beautiful, 
+    or you helped someone,
+    tried something you wereent sure about or took a risk and it worked out well 
+    or you recieved recognition for doing something well
+
+    like if
+    the purpose of this game is to get the user to start thinking and feeling like they are a winner with a tono of money and siccess 
+    ask them user what would a person who always wins do in thie situation
+    or what would it be like if you were a winner or could do whatever you want or had infinite money etc
+
+    math/logic games
+    ask the user to solve basic math problems in their head like multiplication or multiplication etc
+    also ask them riddles and logic word puzzles
+
+
+  `
+
   useEffect(() => {
     // Get available voices
     const loadVoices = () => {
@@ -165,7 +232,7 @@ function AppHome() {
     const body = JSON.stringify({
       model: 'deepseek-chat',
       messages: [
-        { role: 'system', content: 'This is a speech based conversation app. GIve relatively short answers that would be expected during a spoken conversation.' },
+        { role: 'system', content: conversationPreface},
         ...recentMessages
       ],
       stream: false,
